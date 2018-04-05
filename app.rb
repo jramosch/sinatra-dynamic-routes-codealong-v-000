72992b8ca7c02ce675abcf3ecac34baf99c3ae43
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-04-05 19:55:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -20,7 +20,9 @@ class App < Sinatra::Base
   end
   
   get '/multiply/:num1/:num2' do
-    @product = params[:num1] * params[:num2]
+    @num1 = params[:num1]
+    @num2 = params[:num2]
+    "#{@num1 * @num2}"
     "#{@product}"
   end
 end
